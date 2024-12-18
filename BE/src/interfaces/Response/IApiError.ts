5fcb22c5cf6852b7d9329b04e918b6762c7222eb
anh tien <anhtienb237@gmail.com>
2024-12-18 17:39:04 +0700

    Scrape Project - Final no more chaning project - Initial UI

--- BE/src/interfaces/Response/IApiError.ts
+++
@@ -1,7 +0,0 @@
-export interface IApiError {
-  statusCode: number; // Mã trạng thái HTTP
-  message: string; // Thông báo lỗi ngắn gọn
-  details?: string; // Thông tin chi tiết hơn về lỗi (tùy chọn)
-  timestamp: string; // Thời gian lỗi xảy ra
-  path: string; // Endpoint gây ra lỗi
-}
